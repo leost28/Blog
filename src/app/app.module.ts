3cc94ab0fb0b689183e59903359a4863df659524
leost28 <leost28@gmail.com>
2021-11-15 00:24:27 +0100

    Componente Blog y New creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
