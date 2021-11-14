e8dac5e6024bf453f409fddd1958d24940480b3d
leost28 <leost28@gmail.com>
2021-11-15 00:27:04 +0100

    Rutas Blog y New añadidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { NewComponent } from './new/new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'blog', component: BlogComponent },
+  { path: 'new', component: NewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
