7df941d6be967c1051df2badc8bbe3d26f954272
leost28 <leost28@gmail.com>
2021-11-15 02:48:47 +0100

    servicio creado & html de newPost terminado

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-new',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewComponent implements OnInit {
 
-  constructor() { }
+  newPost: FormGroup;
+
+  constructor(
+    private postService: PostService
+  ) {
+
+    this.newPost= new FormGroup ({
+      titulo: new FormControl('', [
+        Validators.required
+      ]),
+      texto: new FormControl('', [
+        Validators.required
+      ]),
+      autor: new FormControl(''),
+      imagen: new FormControl(''),
+      fecha: new FormControl(''),
+      categoria: new FormControl('')
+
+    });
+   }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    console.log(this.newPost.value);
+    this.postService.insertPost(this.newPost.value)
+    
+  }
 }
---
+++ src/app/post.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Post } from './interface/post.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  arrPost: Post[]; 
+
+  constructor() { 
+    this.arrPost= [];
+  }
+
+  insertPost(newPost: any): void{
+    this.arrPost.push(newPost);
+    console.log(this.arrPost);
+    
+
+  }
+}
