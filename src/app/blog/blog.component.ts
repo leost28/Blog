ccb12d2372fcf4238e0b1fae2b213ebb47b48a3c
leost28 <leost28@gmail.com>
2021-11-19 21:53:33 +0100

    FIN BLOG

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,3 +1,4 @@
+import { isGeneratedFile } from '@angular/compiler/src/aot/util';
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../interface/post.interface';
 import { PostService } from '../post.service';
@@ -9,19 +10,66 @@ import { PostService } from '../post.service';
 })
 export class BlogComponent implements OnInit {
 
+
   todosLosPost!: Post[];
+  posts: Post[] = [];
 
   constructor(
     private postService: PostService
   ) {
-
-   }
+  }
 
   ngOnInit(): void {
     this.postService.getAllPosts()
-    .then(arrPost =>{
-      this.todosLosPost = arrPost;
-    })
+      .then(arrPost => {
+        this.todosLosPost = arrPost;
+        this.posts = arrPost;
+      })
+  }
+
+
+
+
+  onCategoriaChange($event: any) {
+    if ($event.target.value === '') {
+      this.posts = this.todosLosPost
+    } else {
+      this.posts = this.todosLosPost.filter(post => post.categoria === $event.target.value);
+    }
+  }
+
+  eliminarPost(post: Post) {
+    if (confirm('¿Estás seguro de que quieres elimnar el Post?')) {
+      this.deletePost(post);
+      localStorage.setItem('posts', JSON.stringify(this.todosLosPost));
+    }
+
+
+  }
+
+  deletePost(post: Post) {
+
+    // for (let i = 0; i < this.todosLosPost.length; i++) {
+    //   if (post === this.todosLosPost[i]) {
+    //     this.todosLosPost.splice(i, 1)
+    //   }
+    // }
+    // for (let i = 0; i < this.posts.length; i++) {
+    //   if (post === this.posts[i]) {
+    //     this.posts.splice(i, 1)
+    //   }
+    // }
+
+    // this.posts.forEach((arrPost : Post, i : number)=> {
+    //   if(arrPost === post){
+    //     this.todosLosPost.splice(i, 1)
+    //   }
+    // })
+
+    
+    this.posts =this.posts.filter(arrPost=>post !== arrPost)
+    this.todosLosPost = this.todosLosPost.filter(arrPost=>post !== arrPost)
+
   }
 
 }
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -7,20 +7,41 @@ import { Post } from './interface/post.interface';
 })
 export class PostService {
 
-  arrPost: Post[]; 
+  arrPost: Post[];
 
-  constructor() { 
-    this.arrPost= [];
+  constructor() {
+    this.arrPost = [];
   }
 
-  agregarPost(newPost: any): void{
-    this.arrPost.push(newPost);
-    console.log(this.arrPost);
+  agregarPost(newPost: Post): void {
+    if (localStorage.getItem('posts') === null) {
+      this.arrPost.push(newPost);
+      localStorage.setItem('posts', JSON.stringify(this.arrPost));
+    } else {
+      this.arrPost = JSON.parse(localStorage.getItem('posts') || '');
+      this.arrPost.push(newPost);
+      localStorage.setItem('posts', JSON.stringify(this.arrPost))
+    }
+
+    // this.arrPost.push(newPost);
+    // console.log(this.arrPost);
   }
 
-  getAllPosts(): Promise<Post[]> { 
-    return new Promise ((resolve, reject) =>{
-      resolve(this.arrPost);
+  getAllPosts(): Promise<Post[]> {
+
+    return new Promise((resolve, reject) => {
+      resolve(JSON.parse(localStorage.getItem('posts') || ''));
     })
   }
+
+  // getByCategoria(categoria: string): Post[] {
+  //   return this.arrPost.filter(post => post.categoria === categoria);
+  // }
+
+
+
+
+
+
 }
+
